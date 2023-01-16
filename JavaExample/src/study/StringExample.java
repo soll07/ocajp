1bffa0ef1436fe1444c7a19a5f617b306194db93
dasol <dasol@218.38.137.28>
2023-01-16 23:48:41 +0900

    StringExample

---
+++ JavaExample/src/study/StringExample.java
@@ -0,0 +1,11 @@
+package study;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안하세요. 오늘은 월요일 입니다.");
+		System.out.println("Hi");
+	}
+
+}
