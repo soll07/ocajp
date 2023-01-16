304f0e86c50926d0287eac03e6086d08a6b9a19b
dasol <dasol@218.38.137.28>
2023-01-16 23:35:35 +0900

    First Commit

---
+++ JavaExample/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dasol
+ *
+ */
+module JavaExample {
+}
---
+++ JavaExample/src/study/NumberExample.java
@@ -0,0 +1,11 @@
+package study;
+
+public class NumberExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(4.2 + 5.6);
+
+	}
+
+}
